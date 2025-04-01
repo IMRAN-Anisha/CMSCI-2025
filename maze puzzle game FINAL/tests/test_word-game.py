1a8225b3f58c9994a8955a9732af73beb35944b6
IMRAN-Anisha <anisha.imran@outlook.com>
2025-04-01 20:16:14 +0800

    unit tests for word game
    milestone 4

---
+++ maze puzzle game FINAL/tests/test_word-game.py
@@ -0,0 +1,96 @@
+import unittest
+from unittest.mock import Mock, patch
+import sys
+import os
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'source')))
+from games.word_game import WordGame  
+
+class TestWordGame(unittest.TestCase):
+    def setUp(self):
+        # Dummy values and stuff
+        self.mock_screen = Mock()
+        self.mock_clock = Mock()
+        self.game = WordGame(self.mock_screen, self.mock_clock)
+        # Reset game state
+        self.game.running = True
+        self.game.guess_count = 0
+        self.game.current_guess = []
+        self.game.guess_string = ""  
+        self.game.game_result = ""
+
+    def test_start_game_sets_word_length(self):
+        # Test if start_game sets the right word length and picks a word
+        with patch("random.choice") as mock_choice:
+            mock_choice.side_effect = ["cat", "cake", "coder"]  # Dummy 
+            self.game.start_game(3)
+            self.assertEqual(self.game.word_length, 3)
+            self.assertEqual(self.game.correct_word, "CAT")
+            self.game.start_game(4)
+            self.assertEqual(self.game.word_length, 4)
+            self.assertEqual(self.game.correct_word, "CAKE")
+            self.game.start_game(5)
+            self.assertEqual(self.game.word_length, 5)
+            self.assertEqual(self.game.correct_word, "CODER")
+
+    def test_add_letter_builds_guess(self):
+        # Test adding letters to the guess
+        self.game.word_length = 3
+        self.game.add_letter("A")
+        self.assertEqual(self.game.guess_string, "A")
+        self.assertEqual(len(self.game.current_guess), 1)
+        self.assertEqual(self.game.current_guess[0]["letter"], "A")
+        self.game.add_letter("B")
+        self.assertEqual(self.game.guess_string, "AB")
+        self.assertEqual(len(self.game.current_guess), 2)
+
+    def test_delete_letter_removes_last(self):
+        # Test deleting the last letter
+        self.game.word_length = 3
+        self.game.add_letter("A")
+        self.game.add_letter("B")
+        self.game.delete_letter()
+        self.assertEqual(self.game.guess_string, "A")
+        self.assertEqual(len(self.game.current_guess), 1)
+        self.assertEqual(self.game.current_guess[0]["letter"], "A")
+
+    def test_check_guess_wins(self):
+        # Test winning with a correct guess
+        self.game.word_length = 3
+        self.game.correct_word = "CAT"
+        self.game.add_letter("C")
+        self.game.add_letter("A")
+        self.game.add_letter("T")
+        self.game.check_guess()
+        self.assertEqual(self.game.game_result, "W")
+        self.assertEqual(self.game.current_guess[0]["color"], "#6aaa64")  
+        self.assertEqual(self.game.current_guess[1]["color"], "#6aaa64")
+        self.assertEqual(self.game.current_guess[2]["color"], "#6aaa64")
+
+    def test_check_guess_loses(self):
+        # Test losing after 6 wrong guesses
+        self.game.word_length = 3
+        self.game.correct_word = "CAT"
+        for _ in range(6):
+            self.game.add_letter("D")
+            self.game.add_letter("O")
+            self.game.add_letter("G")
+            self.game.check_guess()
+            if _ < 5:  # Before the 6th guess
+                self.assertEqual(self.game.game_result, "")
+                self.assertEqual(self.game.guess_count, _ + 1)
+        self.assertEqual(self.game.game_result, "L")
+
+    def test_check_guess_colors(self):
+        # Test coloring (green, yellow, grey)
+        self.game.word_length = 3
+        self.game.correct_word = "CAT"
+        self.game.add_letter("C")  # Right spot
+        self.game.add_letter("T")  # In word, wrong spot
+        self.game.add_letter("X")  # Not in word
+        self.game.check_guess()
+        self.assertEqual(self.game.current_guess[0]["color"], "#6aaa64")  # Green
+        self.assertEqual(self.game.current_guess[1]["color"], "#c9b458")  # Yellow
+        self.assertEqual(self.game.current_guess[2]["color"], "#787c7e")  # Grey
+
+if __name__ == "__main__":
+    unittest.main()
