72616554023cbd664c7c5e307922980153aa19ed
IMRAN-Anisha <anisha.imran@outlook.com>
2025-04-01 17:07:32 +0800

    Dictionaries for WORD GAME
    milestone four

---
+++ maze puzzle game FINAL/source/words.py
@@ -0,0 +1,24 @@
+Three_LETTER = [
+    "ace", "add", "age", "aid", "aim", "air", "and", "ant", "any", "ape",
+    "arc", "arm", "art", "ash", "ask", "ate", "awe", "axe", "bad", "bag",
+    "ban", "bar", "bat", "bay", "bed", "bee", "beg", "bet", "bid", "big",
+    "bin", "bit", "bog", "bow", "box", "boy", "bud", "bug", "bum", "bun",
+    "bus", "buy", "cab", "can", "cap", "car", "cat", "cog", "cop", "cot",
+    "cow", "cry", "cub", "cue", "cup", "cut", "dam", "day", "den", "dew",
+    "dig", "dim", "dip", "dog", "dot", "dry", "due", "dug", "dye", "ear",
+    "eat", "eel", "egg", "elm", "end", "eye", "fan", "far", "fat", "fed",
+    "fee", "few", "fig", "fin", "fit", "fix", "fly", "fog", "foe", "for"
+]
+
+Four_LETTER = [
+    "able", "acid", "aged", "ally", "arch", "area", "army", "aunt", "baby", "back",
+    "bake", "ball", "band", "bank", "bare", "bark", "base", "bath", "bead", "beam",
+    "bean", "bear", "beat", "beer", "bell", "belt", "bend", "best", "bike", "bill",
+    "bird", "bite", "blow", "blue", "boat", "body", "boil", "bold", "bone", "book",
+    "boom", "boot", "born", "boss", "both", "bowl", "burn", "bush", "busy", "cake",
+    "call", "calm", "camp", "cane", "card", "care", "case", "cash", "cast", "cave",
+    "cell", "chat", "chef", "chip", "city", "clap", "clay", "club", "coal", "coat",
+    "code", "coin", "cold", "comb", "come", "cook", "cool", "cope", "copy", "core",
+    "corn", "cost", "crew", "crop", "crow", "cure", "curl", "cute", "damp", "dare",
+    "dark", "dash", "date", "dawn", "deal", "dear", "deck", "deep", "deer", "desk"
+]
